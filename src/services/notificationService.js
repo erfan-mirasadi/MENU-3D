fc2788ab97df2e6516c1191b02a692034e509a63
erfan <erfan.mirasadi@gmail.com>
2026-02-23 02:56:12 +0300

    improve bugs

--- src/services/notificationService.js
+++ src/services/notificationService.js
@@ -1,3 +1,4 @@
+import { supabase } from "@/lib/supabase";
 import { removeUserPushToken, updateUserPushToken } from "@/services/userService";
 
 export async function subscribeToPushNotifications() {
